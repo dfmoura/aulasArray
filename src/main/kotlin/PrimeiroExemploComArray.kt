cf82da3b883ddd0b7ab17ffea42f69e505347ec2
dfmoura <diogomou@gmail.com>
2022-10-08 00:28:21 -0300

    Initial commit

---
+++ src/main/kotlin/PrimeiroExemploComArray.kt
@@ -0,0 +1,7 @@
+package aulasarray
+fun main() {
+    val arrayVazio = emptyArray<Double>()
+    println(arrayVazio)
+    println(arrayVazio.size)
+    //imprime o tipo de dado e o endereço da memória
+}
